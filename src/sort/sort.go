6381a3a11bbdb451ea76d1371a9460791ff7184c
Raunak Jodhawat <raunakjodhawat@gmail.com>
2020-01-04 18:19:01 +0530

    selection sort

--- src/sort/sort.go
+++ src/sort/sort.go
@@ -42,3 +42,29 @@ func RecursiveBubbleSort(input []int, reverse ...bool) []int {
 		return RecursiveBubbleSort(input, reverse...)
 	}
 }
+
+func SelectionSort(input []int, reverse ...bool) []int{
+	defer utility.GetExecutionTime(time.Now(), "SelectionSort")
+	inputCopy := make([]int, len(input), len(input) * 2)
+	if len(input) <= 1 {
+		return input
+	}
+
+	minElement := input[0] // assume, min element at index 0
+	currentPointer := 0 // starting point for inner loop
+	changeIndex := 0 // swapping index
+
+	for i:=0; i<len(input); i++ {
+		// loop to find min element
+		minElement = input[currentPointer]
+		for j:=currentPointer; j< len(input); j++ {
+			if input[j] < minElement {
+				minElement = input[j] // change min element
+				changeIndex = j // store the index of minimum element
+			}
+		}
+		inputCopy[currentPointer], input[changeIndex] = minElement, input[currentPointer]
+		currentPointer++
+	}
+	return Reverse(inputCopy, reverse...)
+}
