aa5a5a2f42b6a11820a85f0e563ca00e3166bf19
Raunak Jodhawat <raunakjodhawat@gmail.com>
2020-05-13 20:36:00 -0400

    Add and AddAll tests

--- src/standardDataStructures/linkedList/linkedList_test.go
+++ src/standardDataStructures/linkedList/linkedList_test.go
@@ -1,1 +1,104 @@
 package linkedlist
+
+import (
+	"reflect"
+	"testing"
+)
+
+func TestListNode_Add(t *testing.T) {
+	list := ListNode{} // creates a instance of linked list node
+	for i := 0; i < 5; i++ {
+		err := list.Add(i) // Adds elements to the list
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+	}
+
+	for i := 0; i < 5; i++ {
+		val, err := list.Get(i)
+		if val != i || err != nil {
+			t.Errorf("expected %v, got %v", i, val)
+		}
+	}
+
+	err := list.Add(10, 0) // Add 10 at index 0
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	val, err := list.Get(0)
+	if val != 10 || err != nil {
+		t.Errorf("expected 10, got %v", val)
+	}
+
+	err = list.Add(11) // Add 11 at end
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	val, err = list.Get(list.Size() - 1)
+	if val != 11 || err != nil {
+		t.Errorf("expected 10, got %v", val)
+	}
+
+	err = list.Add(12, 3) // Add 12 at index 3
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	val, err = list.Get(3)
+	if val != 12 || err != nil {
+		t.Errorf("expected 12, got %v", val)
+	}
+
+	err = list.Add(13, list.Size()+1) // Add 12 at invalid index
+	if err == nil {
+		// Expect a error
+		t.Errorf(err.Error())
+	}
+}
+
+func TestListNode_AddAll(t *testing.T) {
+	list := ListNode{}                       // creates a instance of linked list node
+	err := list.AddAll([]int{0, 1, 2, 3, 4}) // Adds elements to the list
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	for i := 0; i < list.Size(); i++ {
+		val, err := list.Get(i)
+		if val != i || err != nil {
+			t.Errorf("expected %v, got %v", i, val)
+		}
+	}
+
+	err = list.AddAll([]string{"hello", "world"}, 0) // Add at index 0
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	val, err := list.Get(0)
+	if val != "hello" || err != nil {
+		t.Errorf("expected hello, got %v", val)
+	}
+
+	err = list.AddAll([]int{11}) // Add 11 at end
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	val, err = list.Get(list.Size() - 1)
+	if val != 11 || err != nil {
+		t.Errorf("expected 10, got %v", val)
+	}
+
+	err = list.AddAll([]bool{true}, 3) // Add 12 at index 3
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+	val, err = list.Get(3)
+	if reflect.ValueOf(val) == reflect.ValueOf(true) || err != nil {
+		t.Errorf("expected true, got %v", val)
+	}
+
+	err = list.AddAll([]float32{13.34}, list.Size()+1) // Add 12 at invalid index
+	if err == nil {
+		// Expect a error
+		t.Errorf(err.Error())
+	}
+}
