9909e93c23cc3b02046b3bfc9dd7fb58ca0290af
Raunak Jodhawat <raunakjodhawat@gmail.com>
2020-05-14 10:36:00 -0400

    pop, push, remove, removeFirst, removeLast

--- src/standardDataStructures/linkedList/linkedList_test.go
+++ src/standardDataStructures/linkedList/linkedList_test.go
@@ -508,3 +508,123 @@ func TestListNode_PollLast(t *testing.T) {
 		t.Errorf("Next tail element is not 3, got %v", tailElement.Val)
 	}
 }
+
+func TestListNode_Pop(t *testing.T) {
+	list := ListNode{} // creates a instance of linked list node
+	for i := 0; i < 5; i++ {
+		err := list.Add(i) // Adds elements to the list
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+	}
+
+	headVal, err := list.Pop()
+	if err != nil || headVal != 0 {
+		t.Errorf("Head element is expected to be 0, got %v", headVal)
+	}
+
+	headVal, err = list.Pop()
+	if err != nil || headVal != 1 {
+		t.Errorf("Head element is expected to be 0, got %v", headVal)
+	}
+}
+
+func TestListNode_Push(t *testing.T) {
+	list := ListNode{} // creates a instance of linked list node
+	for i := 0; i < 5; i++ {
+		err := list.Add(i) // Adds elements to the list
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+	}
+
+	err := list.Push(10)
+	if err != nil {
+		t.Errorf(err.Error())
+	}
+
+	headElement, err := list.Get(0)
+	if err != nil || headElement != 10 {
+		t.Errorf("Expected head element to be 10, got %v", headElement)
+	}
+}
+
+func TestListNode_Remove(t *testing.T) {
+	list := ListNode{} // creates a instance of linked list node
+	for i := 0; i < 5; i++ {
+		err := list.Add(i) // Adds elements to the list
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+	}
+
+	removedElement, err := list.Remove()
+	if err != nil || removedElement.Val != 0 || removedElement.Prev != nil {
+		t.Errorf("Expected removed element to be 0, got %v", removedElement.Val)
+	}
+
+	newHead, err := list.Get(0)
+	if err != nil || newHead != 1 {
+		t.Errorf("Expected head element to be 1, got %v", newHead)
+	}
+
+	removedElement, err = list.Remove(0)
+	if err != nil || removedElement.Val != 1 || removedElement.Prev != nil {
+		t.Errorf("Expected removed element to be 0, got %v", removedElement.Val)
+	}
+
+	newHead, err = list.Get(0)
+	if err != nil || newHead != 2 {
+		t.Errorf("Expected head element to be 1, got %v", newHead)
+	}
+
+	removedElement, err = list.Remove(list.Size())
+	if err == nil {
+		t.Errorf("Expected error, because list.size is not a valid index, which can be removed")
+	}
+}
+
+func TestListNode_RemoveFirst(t *testing.T) {
+	list := ListNode{} // creates a instance of linked list node
+	for i := 0; i < 5; i++ {
+		err := list.Add(i) // Adds elements to the list
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+	}
+
+	removedElement, err := list.RemoveFirst()
+	if err != nil || removedElement.Val != 0 || removedElement.Prev != nil {
+		t.Errorf("Expected removed element to be 0, got %v", removedElement.Val)
+	}
+
+	newHead, err := list.Get(0)
+	if err != nil || newHead != 1 {
+		t.Errorf("Expected head element to be 1, got %v", newHead)
+	}
+}
+
+func TestListNode_RemoveLast(t *testing.T) {
+	list := ListNode{} // creates a instance of linked list node
+	for i := 0; i < 5; i++ {
+		err := list.Add(i) // Adds elements to the list
+		if err != nil {
+			t.Errorf(err.Error())
+		}
+	}
+
+	removedElement, err := list.RemoveLast()
+	if err != nil || removedElement.Val != 4 || removedElement.Next != nil {
+		t.Errorf("Expected removed element to be 4, got %v", removedElement.Val)
+	}
+
+	newHead, err := list.Get(0)
+	if err != nil || newHead != 0 {
+		t.Errorf("Expected head element to be 0, got %v", newHead)
+	}
+
+	newHead, err = list.Get(list.Size() - 1)
+	if err != nil || newHead != 3 {
+		t.Errorf("Expected tail element to be 3, got %v", newHead)
+	}
+}
